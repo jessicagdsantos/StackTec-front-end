43683203b351656744d6058ca4f3bf2cfddc4948
Weijun001 <69877680+Weijun001@users.noreply.github.com>
2023-05-13 22:32:52 -0300

    Add files via upload

---
+++ Tela sem login IV/script.js
@@ -0,0 +1,34 @@
+const footerText = document.getElementById('footer-text');
+let intervalID;
+
+function typeFooterText() {
+  const text = 'Feito por um bando de Guaxinins Raivosos';
+  let i = 0;
+
+  intervalID = setInterval(() => {
+    if (i < text.length) {
+      footerText.textContent += text[i];
+      i++;
+    } else {
+      clearInterval(intervalID);
+    }
+  }, 100);
+}
+
+function clearFooterText() {
+  footerText.textContent = '';
+}
+
+window.addEventListener('beforeunload', () => {
+  clearInterval(intervalID);
+  clearFooterText();
+});
+
+window.addEventListener('load', () => {
+  typeFooterText();
+  setInterval(() => {
+    clearFooterText();
+    typeFooterText();
+  }, 10000);
+});
+
